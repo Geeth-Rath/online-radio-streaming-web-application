fda0cd4a3765bb1e983d7a71b82689c8faeed103
Geeth-Rath <geeth4862@gmail.com>
2024-05-31 15:28:19 +0530

    change rate feild in api

--- backend/src/main/java/com/onlineRadio/backend/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.onlineRadio.backend.repository;
-//
-//import com.onlineRadio.backend.model.Image;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ImageRepository extends JpaRepository<Image, Integer> {
-//}
